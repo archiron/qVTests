4a95993bcd50adb11fd23938b0f8ab2b78a47d25
archiron <arnaud.chiron@cern.ch>
2020-11-04 17:53:51 +0100

    version 0.50
    a lot of corrections : add multiset capability for multiples instances of validations.
    add a correction of miniAOD bug into reference value and others minor corrections.

--- functionsqV.py
+++ functionsqV.py
@@ -302,6 +302,8 @@ def get_answer4(text2prompt):
     while ( quitLoop ):
         rel = raw_input(text2prompt)
         #rel = input(text2prompt)  # Python 3
+
+        print('rel : %s' % rel)
         if rel == 'q':
             exit()
         elif rel == 'b':
@@ -376,19 +378,29 @@ def get_answer6(text2prompt, tab): # for GT
 def fonction_1(self):
     screen_clear()
     print('vous appelez la fonction 1')
-    print_tab_2(self.web_location, self.color_nb)
-    #print('==')
 
+    print_tab_2(self.web_location, self.color_nb)
     text_to_prompt= "number of the folder choice or [" + colorText('q', self.color) +"]uit. ? "
     self.location, self.extension = get_answer1(text_to_prompt, self.web_location, self.web_extension)
     print('WEB LOCATION : %s' % colorText(self.location, 'blue')) # now we have the web folder location
     #self.gev_tmp.append(self.location) # seems to be inefficient
     sleep(1)
+
     return 1
 
 def fonction_2(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 2')
+    # initialization
+    self.default_dataset = []
+    self.datasets = []
+    self.DB_flags = []
+    self.gev_tmp = []
+    self.releasesList_5 = []  # root files list for release after validation choice
+    self.referencesList_5 = []  # root files list for reference after validation choice
+    self.releasesGT = []
+    self.referencesGT = []
+
     # get the list for RELEASE
     print_tab_1(self.list_0, self.color_nb)
 
@@ -410,7 +422,7 @@ def fonction_2(self):
     return 2
 
 def fonction_3(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 3')
     print_tab_1(self.releasesList_2, self.color_nb)
     text_to_prompt= "number of the RELEASE, [" + colorText('b', self.color) + "]ack or [" + colorText('q', self.color) +"]uit. ? "
@@ -427,7 +439,7 @@ def fonction_3(self):
         return 1
 
 def fonction_4(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 4')
     text_to_prompt= "Reference family for the validation "
     print(text_to_prompt)  #
@@ -453,7 +465,7 @@ def fonction_4(self):
         return 2
 
 def fonction_5(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 5')
     print_tab_1(self.referencesList_2, self.color_nb)
     text_to_prompt= "number of the REFERENCE, [" + colorText('b', self.color) + "]ack or [" + colorText('q', self.color) +"]uit. ? "
@@ -464,12 +476,14 @@ def fonction_5(self):
         print('')
         self.referencesList_3 = sub_releases3(self.reference, self.referencesList_1) # extract the list of the root files for the chosen reference
         self.datasetsList_2 = sub_releases2(self.reference, self.referencesList_3) # extract the datasets of the root files for the chosen reference
+
+        #print(self.referencesList_3)
         return 5
     else:
         return 3
 
 def fonction_6(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 6')
     # web folder name customization
     # text to add into folder name for release
@@ -489,7 +503,7 @@ def fonction_6(self):
             return 6
 
 def fonction_7(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 7')
     # web folder name customization
     # text to add into folder name for reference
@@ -509,7 +523,7 @@ def fonction_7(self):
             return 7
 
 def fonction_8(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 8')
     # comparison choice
     print_tab_4(self.comparisons, self.color_nb)
@@ -520,18 +534,26 @@ def fonction_8(self):
     return 8
 
 def fonction_9(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 9')
     # validation choice
     print_tab_4(self.validations, self.color_nb)
     text_to_prompt= "number of the validation type, [" + colorText('b', self.color) + "]ack or [" + colorText('q', self.color) +"]uit. ? "
 
     self.validationChoice = get_answer3(text_to_prompt, self.validations) #
+    if ( self.validationChoice[0] == 'RECO' and self.validationChoice[1] == 'miniAOD' ):
+        self.reference = self.release
+        self.referencesList_3 = self.releasesList_3
+        self.datasetsList_2 = self.datasetsList_1
+    if ( self.validationChoice[0] == 'pmx' and self.validationChoice[1] == 'PU' ):
+        self.reference = self.release
+        self.referencesList_3 = self.releasesList_3
+        self.datasetsList_2 = self.datasetsList_1
 
     return 9
 
 def fonction_10(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 10')
     # datasets choice
     fieldname = 'DataSetsFilter_' + self.comparisonChoice[0] + 'vs' + self.comparisonChoice[1] + self.validationChoice[0]
@@ -546,6 +568,7 @@ def fonction_10(self):
     text_to_prompt += "you can [" + colorText('s', self.color) + "]elect datasets from the list, or [" + colorText('c', self.color) + "]hose between the common datasets : "
 
     self.dts = get_answer4(text_to_prompt) #
+    self.datasets = []
     if ( self.dts == ''): # back
         return 8
     elif ( self.dts == 's'): # default
@@ -555,7 +578,6 @@ def fonction_10(self):
         function_102(self)
         return 10
     elif ( self.dts == 'd'): # default
-        self.datasets = []
         for item in self.default_dataset:
             if item[1] == 1:
                 self.datasets.append(item[0])
@@ -566,20 +588,21 @@ def fonction_10(self):
     return 10
 
 def function_101(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 101')
-    print_tab_3(self.default_dataset, 'blue') # all datasets with same color
+    print_tab_3(self.default_dataset, self.color_nb) # all datasets with same color
     text_to_prompt= "enter the numbers of the datasets you want, separated by commas : "
     numbers = get_answer10X(text_to_prompt, self.default_dataset)
-    #print(numbers)
+    print(numbers)
+    self.datasets = []
     for i in numbers:
         self.datasets.append(str(self.default_dataset[i][0]))
-    print('datasets : ')
+    print('datasets 101 : ')
     print(self.datasets)
     return
 
 def function_102(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 102')
 
     # peut etre un test a faire si len(commonDatasets) = 0 !
@@ -593,15 +616,22 @@ def function_102(self):
     #print(numbers)
     for i in numbers:
         self.datasets.append(str(self.commonDatasets[i]))
-    print('datasets : ')
+    print('datasets 102 : ')
     print(self.datasets)
     return
 
 def fonction_11(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 11')
+    self.releasesGT = []
+    self.referencesGT = []
+    self.relRootFilesList = []
+    self.refRootFilesList = []
+    self.releasesList_4 = []  # root files list for release
+    self.referencesList_4 = []  # root files list for reference
 
     # get the self.releasesList_4/self.referencesList_4 lists of root files
+    print('root files extraction')
     rootFilesExtraction(self)
 
     # extract to keep only for comparison choice
@@ -619,12 +649,18 @@ def fonction_11(self):
     # extract to keep only for validation choice
     print('validation choice for release')
     self.releasesList_5 = getFilesList(self, self.releasesList_4) # tmp_list_rel
-
+    #print(self.releasesList_5)
     print('validation choice for reference')
     self.referencesList_5 = getFilesList(self, self.referencesList_4) # tmp_list_ref
+    #print(self.referencesList_4)
 
     # get the self.releasesGT/self.referencesGT lists for GT
+    print('GT extraction')
     GlobalTagsExtraction(self)
+    #print('self.releasesGT')
+    #print(self.releasesGT)
+    print('self.referencesGT')
+    print(self.referencesGT)
 
     # rewrite GT list in a more convenient way
     tmp_GT = []
@@ -642,14 +678,14 @@ def fonction_11(self):
                 if item1[i] == elem:
                     tmp.append(item1[0])
         self.GT_rel.append(tmp)
-    #print(self.GT_rel)
+    print(self.GT_rel)
     # reduce the list to kept only those corresponding to ALL datasets
     tt_rel = []
     for elem in self.GT_rel:
         if len(elem[1:]) == len(self.datasets):
             tt_rel.append(elem)
     self.GT_rel = tt_rel
-    #print(self.GT_rel)
+    print(self.GT_rel)
 
     tmp_GT = []
     for elem in self.referencesGT:
@@ -666,16 +702,17 @@ def fonction_11(self):
                 if item1[i] == elem:
                     tmp.append(item1[0])
         self.GT_ref.append(tmp)
-    #print(self.GT_ref)
+    print(self.GT_ref)
     # reduce the list to kept only those corresponding to ALL datasets
     tt_ref = []
     for elem in self.GT_ref:
         if len(elem[1:]) == len(self.datasets):
             tt_ref.append(elem)
     self.GT_ref = tt_ref
-    #print(self.GT_ref)
+    print(self.GT_ref)
 
     if len(self.GT_rel) == 0 or len(self.GT_ref) == 0 :
+        print(colorText('len(self.GT_rel) == 0 or len(self.GT_ref) == 0', 'red'))
         return 9 # back to datasets choice
 
     print_tab_5(self.GT_rel, self.color_nb)
@@ -694,7 +731,7 @@ def fonction_11(self):
         return 11
 
 def fonction_12(self):
-    screen_clear()
+    #screen_clear()
     print('vous appelez la fonction 12')
 
     print_tab_5(self.GT_ref, self.color_nb)
@@ -727,6 +764,7 @@ def fonction_13(self):
     if DB_flag == '':
         return 11
     elif DB_flag == 'y':
+        self.DB_flags = []
         for item in self.datasets:
             if item != 'ZEE_14':
                 self.DB_flags.append('False')
@@ -752,6 +790,7 @@ def fonction_14(self):
     print(self.relRootFilesList)
     print(self.refRootFilesList)
     print('')
+    # include the differents choices into Gev
     self.gev_tmp.append([str(self.release), str(self.reference)])
     self.gev_tmp.append([str(self.releaseExtent), str(self.referenceExtent)])
     self.gev_tmp.append(str(self.datasets))
@@ -761,38 +800,54 @@ def fonction_14(self):
     self.gev_tmp.append(str(self.DB_flags))
     print(self.gev_tmp)
     self.Gev.append(self.gev_tmp)
-    print(self.Gev)
+    #print(self.Gev)
 
-    if self.configFile:
-        print('config file OK')
-        self.configFile.write('#! /usr/bin/env python\n')
-        self.configFile.write('#-*-coding: utf-8 -*-\n')
-        self.configFile.write('\n')
-        self.configFile.write('import os,sys\n')
-        self.configFile.write('\n')
-        self.configFile.write('#############################################################################\n')
-        self.configFile.write('# global data\n')
-        self.configFile.write('web_repo = ' + str([self.location, self.extension]) + '\n')
-        self.configFile.write('\n')
-        ind = 0
-        for elem in self.Gev:
-            self.configFile.write('# personalization ' + str(ind + 1) + '\n')
-            self.configFile.write('GeV_' + str(ind + 1) + ' = [\n')
-            self.configFile.write(str(unicode(elem[0])) + ' , # release/reference\n')
-            self.configFile.write(str(elem[1]) + ' , # relref_extent\n')
-            self.configFile.write(str(elem[2]) + ' , # datasets\n')
-            self.configFile.write('\'' + str(elem[3]) + '\' , # choice\n')
-            self.configFile.write(str(elem[4]) + ' , # relrefValtype RECO vs RECO\n')
-            self.configFile.write(str(elem[5]) + ' , # GT one couple rel/ref for all dataset\n')
-            self.configFile.write('[\'\', \'\'] , # relref files one couple rel/ref for each dataset\n')
-            self.configFile.write(str(self.DB_flags) + ', # DB flag\n')
-            self.configFile.write('\n')
-        self.configFile.write('#############################################################################\n')
-        self.configFile.write('\n')
-    else:
-        print('no config file open')
-        exit()
     return 14
+
+def fonction_15(self):
+    #screen_clear()
+    print('vous appelez la fonction 15')
+    # New set ?
+    text_to_prompt = "Adding another set of validation ?, [" + colorText('y', self.color) + "]es/["
+    text_to_prompt += colorText('n', self.color) + "o], [" + colorText( 'b', self.color)
+    text_to_prompt += "]ack or [" + colorText('q', self.color) + "]uit. ? "
+    answer4new = get_answer5(text_to_prompt)  #
+    print('answer for new set : %s' % answer4new)
+    if answer4new == '': # back
+        return 12
+    elif answer4new == 'y': # back to release choice
+        return 1
+    elif answer4new == 'n': # so we close after writing into config file
+        if self.configFile:
+            print('config file OK')
+            self.configFile.write('#! /usr/bin/env python\n')
+            self.configFile.write('#-*-coding: utf-8 -*-\n')
+            self.configFile.write('\n')
+            self.configFile.write('import os,sys\n')
+            self.configFile.write('\n')
+            self.configFile.write('#############################################################################\n')
+            self.configFile.write('# global data\n')
+            self.configFile.write('web_repo = ' + str([self.location, self.extension]) + '\n')
+            self.configFile.write('\n')
+            ind = 0
+            for elem in self.Gev:
+                self.configFile.write('# personalization ' + str(ind + 1) + '\n')
+                self.configFile.write('GeV_' + str(ind + 1) + ' = [\n')
+                self.configFile.write(str(unicode(elem[0])) + ' , # release/reference\n')
+                self.configFile.write(str(elem[1]) + ' , # relref_extent\n')
+                self.configFile.write(str(elem[2]) + ' , # datasets\n')
+                self.configFile.write('\'' + str(elem[3]) + '\' , # choice\n')
+                self.configFile.write(str(elem[4]) + ' , # relrefValtype RECO vs RECO\n')
+                self.configFile.write(str(elem[5]) + ' , # GT one couple rel/ref for all dataset\n')
+                self.configFile.write('[\'\', \'\'] , # relref files one couple rel/ref for each dataset\n')
+                self.configFile.write(str(self.DB_flags) + ', # DB flag\n')
+                self.configFile.write('\n')
+            self.configFile.write('#############################################################################\n')
+            self.configFile.write('\n')
+        else:
+            print('no config file open')
+            exit()
+        return 15
 
 def testZEE(tab):
     test = False
@@ -804,26 +859,34 @@ def testZEE(tab):
 def rootFilesExtraction(self):
     # ROOT files extraction for selected datasets
     for i, dts in enumerate(self.datasets):
+        print('dts : %s' % dts)
         tmp_rel = []
         tmp_ref = []
         tmp_rel.append(dts)
         tmp_ref.append(dts)
+        print('releasesList_3')
         for file in self.releasesList_3:
+            print(file)
             if re.search(dts, file):
                 if re.search(self.release, file):
-                    #print(file)
+                    print('rel ', file)
                     tmp_rel.append(str(file))
-                else:
-                    tmp_ref.append(str(file))
+                #else:
+                #    print('ref ', file)
+                #    tmp_ref.append(str(file))
+        print('referencesList_3')
         for file in self.referencesList_3:
+            print(file)
             if re.search(dts, file):
-                if re.search(self.release, file):
-                    print(file)
-                    tmp_rel.append(str(file))
-                else:
+                if re.search(self.reference, file):
+                    print('ref ', file)
                     tmp_ref.append(str(file))
+                #else:
+                #    print('ref ', file)
+                #    tmp_ref.append(str(file))
         self.releasesList_4.append(tmp_rel)
         self.referencesList_4.append(tmp_ref)
+        #print(self.referencesList_4)
 
 def GlobalTagsExtraction(self):
     #print('longueur : %d' % len(self.releasesList_5))
--- quickVal.py
+++ quickVal.py
@@ -7,7 +7,7 @@ from time import sleep
 
 sys.path.append('/afs/cern.ch/user/a/archiron/lbin/ChiLib')
 
-from networkFunctions import list_search_0, list_search_1
+from networkFunctions import list_search_0
 from functionsqV import *
 from defaultqV import *
 
@@ -35,87 +35,23 @@ class quickVal():
         self.validations = validations
         self.releaseExtent = ''
         self.referenceExtent = ''
-        self.default_dataset = []
-        self.datasets = []
-        self.DB_flags = []
         self.web_location = [DataLocation(self)[0][2],
                            DataLocation(self)[1][2]]
         self.web_extension = [DataLocation(self)[0][3],
                            DataLocation(self)[1][3]]
         self.Gev = [] # table for validations
-        self.gev_tmp = []
-        self.releasesList_4 = [] # root files list for release
-        self.referencesList_4 = [] # root files list for reference
-        self.releasesList_5 = [] # root files list for release after validation choice
-        self.referencesList_5 = [] # root files list for reference after validation choice
-        self.releasesGT = []
-        self.referencesGT = []
-        self.relRootFilesList = []
-        self.refRootFilesList = []
 
         print('config.py file creation')
         try:
-            self.configFile = open('configFile.py', 'w+') #
+            self.configFile = open('configFile.py', 'w') #
         except IOError:
             print("Could not open file!")
 
         i_back = 1
         while (i_back != 0):
             table=getattr(sys.modules[__name__], "fonction_%s" % str(i_back))(self)
-            i_back = (table + 1) % 15
+            i_back = (table + 1) % 16
             print('i_back : %d' % i_back)
-            # include the differents choices into Gev
-
-        #screen_clear()
-        '''print('RESUME : ')
-        text_to_prompt = 'RELEASE : ' + colorText(self.release, 'blue') + ' - REFERENCE : ' + colorText(self.reference, 'blue')
-        print(text_to_prompt)
-        text_to_prompt = 'release extension : ' + colorText(self.releaseExtent, 'blue') + ' - reference extension : ' + colorText(self.referenceExtent, 'blue')
-        print(text_to_prompt)
-        print(self.GT_rel)
-        print(self.GT_ref)
-        print(self.relRootFilesList)
-        print(self.refRootFilesList)
-        print('')
-        self.gev_tmp.append([str(self.release), str(self.reference)])
-        self.gev_tmp.append([str(self.releaseExtent), str(self.referenceExtent)])
-        self.gev_tmp.append(str(self.datasets))
-        self.gev_tmp.append(str(self.comparisonChoice))
-        self.gev_tmp.append(str(self.validationChoice))
-        self.gev_tmp.append([str(self.GT_rel), str(self.GT_ref)])
-        self.gev_tmp.append(str(self.DB_flags))
-        print(self.gev_tmp)
-        self.Gev.append(self.gev_tmp)
-        print(self.Gev)
-
-        print('suite ...')
-        #stop
-
-        if self.configFile:
-            self.configFile.write('#! /usr/bin/env python\n')
-            self.configFile.write('#-*-coding: utf-8 -*-\n')
-            self.configFile.write('\n')
-            self.configFile.write('import os,sys\n')
-            self.configFile.write('\n')
-            self.configFile.write('#############################################################################\n')
-            self.configFile.write('# global data\n')
-            self.configFile.write('web_repo = ' + str([self.location, self.extension]) + '\n')
-            self.configFile.write('\n')
-            ind = 0
-            for elem in self.Gev:
-                self.configFile.write('# personalization ' + str(ind+1) + '\n')
-                self.configFile.write('GeV_'+ str(ind+1) +' = [\n')
-                self.configFile.write(str(unicode(elem[0])) + ' , # release/reference\n')
-                self.configFile.write(str(elem[1]) + ' , # relref_extent\n')
-                self.configFile.write(str(elem[2]) + ' , # datasets\n')
-                self.configFile.write('\'' + str(elem[3]) + '\' , # choice\n')
-                self.configFile.write(str(elem[4]) + ' , # relrefValtype RECO vs RECO\n')
-                self.configFile.write(str(elem[5]) + ' , # GT one couple rel/ref for all dataset\n')
-                # ROOT files
-                self.configFile.write(str(self.DB_flags) + ', # DB flag\n')
-                self.configFile.write('\n')
-            self.configFile.write('#############################################################################\n')
-            self.configFile.write('\n')'''
 
         self.configFile.close()
         print('... et fin')
